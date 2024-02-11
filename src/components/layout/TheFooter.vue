e02694fe5c4babe1bda295cc87aad63c80b4e2fd
trancaodua <trancaodua199@gmail.com>
2024-02-11 20:59:47 +0700

    Add file

--- src/components/layout/TheFooter.vue
+++ src/components/layout/TheFooter.vue
@@ -5,26 +5,170 @@
             <p>Create professional-looking resumes and easy to use in a fraction of the time. The greatest online resume
                 builder can provide you a significant advantage in the job market.</p>
         </div>
-        <div class="menu-container">2</div>
-        <div class="contact-contianer">3</div>
+        <div class="menu-wrapper">
+            <ul class="menu-container">
+                <li><router-link class="link" :to="{ name: 'home' }">Home</router-link> </li>
+                <li><router-link class="link" :to="{ name: 'contact' }">Contact</router-link> </li>
+            </ul>
+            <ul class="social-container">
+                <li>
+                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                </li>
+                <li>
+                    <a href="#"> <i class="fa-brands fa-skype"></i></a>
+                </li>
+                <li>
+                    <a href="#"> <i class="fa-brands fa-twitter"></i></a>
+                </li>
+            </ul>
+        </div>
+        <div class="contact-contianer">
+            <h3>Connect with me</h3>
+            <form>
+                <div class="form-control">
+                    <label for="email">Email</label>
+                    <input type="text" placeholder="Enter your email" name="email">
+                </div>
+                <div class="form-control">
+                    <label for="email">Message</label>
+                    <input type="text" placeholder="Message" name="message">
+                </div>
+                <button type="submit">Send</button>
+            </form>
+        </div>
     </footer>
 </template>
 <style scoped>
 footer {
-    padding: 1rem;
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     grid-auto-rows: 1 auto;
     background-color: #F8F9FE;
-    padding:5rem;
+    padding: 3rem;
 }
 
-.copyring-contaner .logo{
-    width:10rem;
+.copyring-contaner {
+    border-right: 1px solid #1F9F96;
 }
 
-.logo img{
-width: 100%;
+.copyring-contaner .logo {
+    width: 10rem;
 }
 
+.copyring-contaner p {
+    color: #1F9F96;
+    font-family: "Rubik", sans-serif;
+    font-size: 1rem;
+    line-height: 2rem;
+    text-align: left;
+}
+
+.logo img {
+    width: 100%;
+}
+
+.menu-wrapper {
+    padding: 1rem;
+    border-right: 1px solid #1F9F96;
+}
+
+.menu-container {
+    list-style: none;
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    gap: 2rem;
+    padding-bottom: 1rem;
+}
+
+.menu-container .link {
+    color: #1F9F96;
+    text-decoration: none;
+    font-size: 1.2rem;
+    font-weight: bold;
+}
+
+.social-container {
+    list-style: none;
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
+    align-items: center;
+    justify-content: center;
+}
+
+.social-container a {
+    font-size: 1rem;
+    color: #1F9F96;
+    border-radius: 50%;
+    padding: 0.2rem;
+    border: 1px solid #1F9F96;
+}
+
+.contact-contianer {
+    width: 100%;
+    padding: 1rem;
+    border-right: 1px solid #1F9F96;
+}
+
+.contact-contianer form {
+    width: 100%;
+    gap: 1rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.form-control {
+    display: grid;
+    grid-template-columns: auto 1fr;
+}
+
+.contact-contianer form input {
+    height: 45px;
+    background: #fff;
+    border-radius: 30px;
+    padding: 0 20px;
+    border: none;
+    outline: none;
+}
+
+.contact-contianer form label {
+    margin: auto;
+    color: #1F9F96;
+    width: 5rem;
+}
+
+.contact-contianer form button {
+    width: 5rem;
+    height: 2rem;
+    border: none;
+    background-color: #1F9F96;
+    color: #fff;
+    cursor: pointer;
+    border-radius: 0.4rem;
+}
+
+.contact-contianer h3 {
+    margin-bottom: 1rem;
+    color: #1F9F96;
+}
+
+@media screen and (max-width:800px) {
+    footer {
+        grid-template-columns: auto;
+        grid-auto-rows: repeat(3, auto);
+    }
+}
+
+/* @media screen and (max-width:800px) {
+    footer {
+        grid-template-columns: repeat(2, auto);
+        grid-auto-rows: repeat(2, auto);
+    }
+
+} */
 </style>
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,3 +1,82 @@
 <template>
-    Login
+    <div class="container">
+        <form>
+            <h2>Login</h2>
+            <div class="control-form">
+                <label for="email">Email Address*</label>
+                <input type="email" name="email">
+            </div>
+            <div class="control-form">
+                <label for="password">Password*</label>
+                <input type="password" name="password">
+            </div>
+            <button>Log In</button>
+        </form>
+    </div>
 </template>
+
+<style scoped>
+.container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+form {
+    max-width: 40rem;
+    width: 100%;
+    padding: 2rem;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+    gap: 2rem;
+}
+
+form h2 {
+    font-size: 2rem;
+    font-family: "Poppins", sans-serif;
+}
+
+.control-form {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    width: 100%;
+}
+
+.control-form label {
+    font-family: "Rubik", sans-serif;
+    font-weight: normal;
+    font-style: normal;
+    color: #565872;
+    font-size: 1.1rem;
+    line-height: 1.8rem;
+}
+
+.control-form input {
+    font-family: "Rubik", sans-serif;
+    font-weight: normal;
+    font-style: normal;
+    color: #565872;
+    font-size: 1.2rem;
+    height: 3rem;
+    width: 100%;
+    border: 1px solid #1574F6;
+    border-radius: none;
+}
+
+form button {
+    width: 5rem;
+    height: 2rem;
+    border: none;
+    outline: none;
+    color:white;
+    background-color: #1F9F96;
+    cursor: pointer;
+    font-size:1.2rem;
+    padding: 0.2rem;
+    font-weight: bold;
+}
+</style>
